a3aa6bd5ec9aa1ef052594d274a6c25965e863d1
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-04-22 21:44:38 -0400

    init

--- relation_matching/modules.py
+++ relation_matching/modules.py
@@ -16,7 +16,7 @@ def init_from_config(args):
     global wiki_url
     config_options = globals.config
 
-    w2v = Word2Vec.init_from_config(config_options)
+    #w2v = Word2Vec.init_from_config(config_options)
     sparql_backend = globals.get_sparql_backend(config_options)
-    entity_linker = EntityLinker.init_from_config()
-    facts_ranker = Ranker.init_from_config(config_options)
+    #entity_linker = EntityLinker.init_from_config()
+    #facts_ranker = Ranker.init_from_config(config_options)
