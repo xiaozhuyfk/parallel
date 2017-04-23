3b98c8b233e33dedbf93d62292da96435137b139
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-04-22 21:52:06 -0400

    init

--- sparql_backend/backend_util.py
+++ sparql_backend/backend_util.py
@@ -6,9 +6,9 @@ def get_id_with_url(backend, url):
     query = '''
         PREFIX fb: <http://rdf.freebase.com/ns/>
         SELECT ?s where {
-            ?s fb:common.topic.topical_webpage %s
+            m.0100s3s6 fb:common.topic.topical_webpage ?s
         }
     '''
 
-    mid = backend.query(query % url)
+    mid = backend.query(query)
     return mid
